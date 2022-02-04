099298b28073e1de335a4a1ced00fb9856afb05e
sum02dean <sum02dean@gmail.com>
2022-02-04 13:57:49 +0100

    updated environmeent.yml file

--- src/run_bambi.sh
+++ src/run_bambi.sh
@@ -2,14 +2,14 @@
 /mnt/mnemo5/sum02dean/miniconda3/envs/string_score/bin/python bambi_model.py \
 --output_dir models/03_02_2022/ \
 --model_name bambi \
---species_id '511145' \
+--species_id '511145 9606 4932' \
 --cogs True \
 --use_noise True \
 --class_weight 4 \
 --neg_ratio 4 \
 --drop_homology True \
 --use_foi False \
---n_samples 1 \
+--n_samples 3 \
 --n_chains 4 \
 --n_draws  1000 \
 --n_tune 3000 \
