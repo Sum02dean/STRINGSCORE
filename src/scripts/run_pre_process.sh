7fe41e2aaaaf63011cd7710dfffc7eecc5849f1c
sum02dean <sum02dean@gmail.com>
2022-04-21 14:58:21 +0200

    update handover

--- src/scripts/run_pre_process.sh
+++ src/scripts/run_pre_process.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 /mnt/mnemo5/sum02dean/miniconda3/envs/string-score-env/bin/python ../pre_process.py \
---input_dir ../pre_processed_data/ \
 --output_dir '../pre_processed_data/' \
 --species_id '511145 4932 9606' \
 --neg_ratio 4 \
