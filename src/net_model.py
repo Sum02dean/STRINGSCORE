3f573efaef6b1edff9aac7a1057862c027ffe3f5
sum02dean <sum02dean@gmail.com>
2022-04-22 17:54:28 +0200

    fixed layer iteration in net

--- src/net_model.py
+++ src/net_model.py
@@ -13,7 +13,14 @@ import argparse
 import subprocess
 import copy
 from collections import Counter as C
+from itertools import tee
 
+
+def pairwise(iterable):
+    # Chains sequences: 'ABCDEFG' --> AB BC CD DE EF FG
+    a, b = tee(iterable)
+    next(b, None)
+    return list(zip(a, b))
 class BinaryClassification(nn.Module):
     def __init__(self, input_dim=11, hidden_dim=[11], output_dim=1):
         """ A Neural Network which can be adaptively parameterised for tabular data predictions.
